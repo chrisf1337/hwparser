15a69e5d296e9aa48aa8f144bf7fc7f39695c9ba
Christopher Fu <chrisf1337@gmail.com>
2013-09-07 19:36:05 -0700

    simple_parse() is now parse()
    We can directly parse homework text by looking at .stripped_strings,
    which simplifies the process and requires only one pass through the html
    source. The script searches for a div style, which is going to break if
    there is any change in how the homework blocks get displayed. The method
    I used to sort the assignments by date is also kind of hacky and
    probably could be improved.

--- hwparser.py
+++ hwparser.py
@@ -2,63 +2,44 @@ import sys
 from bs4 import BeautifulSoup
 import re
 import argparse
+import datetime
+from operator import itemgetter
 # import urllib2
 
-def simple_parse(filename, output='out.txt'):
+def parse(filename, output='out.txt'):
     soup = BeautifulSoup(open(filename, 'r').read())
     out = open(output, 'w')
     hw_content_stripped = [tag for tag in
             soup.find_all('div', style =
             'margin: 5px 5px 0px 5px; font-size: 12px; padding-bottom: 10px;')]
+    hw_content = []
     for tag in hw_content_stripped:
+        assignment = []
         for string in tag.stripped_strings:
+            assignment.append(string)
+        hw_content.append(assignment)
+    for assignment in hw_content:
+        if '- ' in assignment[0]:
+            p = re.compile(r'- ([0-9]{1,2})/([0-9]{1,2})/([0-9]{4})')
+            m = p.search(assignment[0])
+            assignment[0] = datetime.date(int(m.group(3)), int(m.group(1)),
+                    int(m.group(2)))
+        else:
+            p = re.compile(r'^([0-9]{1,2})/([0-9]{1,2})/([0-9]{4})')
+            m = p.search(assignment[0])
+            assignment[0] = datetime.date(int(m.group(3)), int(m.group(1)),
+                    int(m.group(2)))
+    hw_content.sort(key=itemgetter(0))
+    for assignment in hw_content:
+        assignment[0] = assignment[0].strftime('%m/%d/%Y')
+    for hw in hw_content:
+        for string in hw:
             out.write(string.encode('utf-8') + '\n')
         out.write('\n')
-
-def parse(filename, output='out.txt'):
-    soup = BeautifulSoup(open(filename, 'r').read())
-    out = open(output, 'w')
-
-    hw_dates = [tag.string for tag in
-            soup.find_all('span', id = re.compile(r'.*StartingOn$'))]
-    hw_titles = [tag.string for tag in
-            soup.find_all('span', id = re.compile(r'.*lblTitle$'))]
-    hw_content = [tag.get_text().strip() for tag in
-            soup.find_all('div', style =
-            'margin: 5px 5px 0px 5px; font-size: 12px; padding-bottom: 10px;')]
-    hw_due_dates = []
-    for date in hw_dates:
-        if '- ' in date:
-            p = re.compile(r'- ([0-9]{1,2}/[0-9]{1,2}/[0-9]{4})')
-            m = p.search(date)
-            hw_due_dates.append(m.group(1))
-        else:
-            p = re.compile(r'^([0-9]{1,2}/[0-9]{1,2}/[0-9]{4})')
-            m = p.search(date)
-            hw_due_dates.append(m.group(0))
-    p = re.compile(r'\n\n\n\n\s*(.*)$')
-    hw_info = []
-    for content in hw_content:
-        m = p.search(content)
-        if m != None:
-            hw_info.append(m.group(1))
-        else:
-            hw_info.append('')
-    if len(hw_due_dates) != len(hw_titles) or len(hw_due_dates) != len(hw_info):
-        raise Exception('lengths don\'t match up')
-    for i in range(len(hw_due_dates)):
-        out.write(hw_due_dates[i] + ': ' + hw_titles[i] + '\n')
-        out.write(hw_info[i].encode('utf-8') + '\n\n')
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('filename', help='the source html file')
     parser.add_argument('output', help='the output file')
-    parser.add_argument('-s', '--simple', help='dump text without sorting',
-            action='store_true')
     args = parser.parse_args()
-    if args.simple:
-        simple_parse(args.filename, args.output)
-    else:
-        parse(args.filename, args.output)
+    parse(args.filename, args.output)
